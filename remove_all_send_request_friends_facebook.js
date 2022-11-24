74de96a4c3760d64db732b25044eb254b9405679
tungnt <tungnt.mmo@gmail.com>
2022-11-24 11:09:28 +0700

    git init

---
+++ remove_all_send_request_friends_facebook.js
@@ -0,0 +1,10 @@
+/**
+ * script remove all send request friends facebook
+ * b1>  Go to link: [https://m.facebook.com/friends/center/requests/outgoing/#friends_center_main]
+ * (https://m.facebook.com/friends/center/requests/outgoing/#friends_center_main)
+ * b2> Press F12 to open Console
+ * Paste this command under in to console: 
+ */
+
+Object.values(document.getElementsByClassName('_54k8 _52jg _56bs _26vk _2b4n _56bt'))
+  .forEach((button) => button.click());
