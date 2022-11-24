4302a698e36fa659525f292dcd1de2a87f0ada45
TungJeny <39626620+TungJeny@users.noreply.github.com>
2022-11-24 14:32:40 +0700

    Revert "unfollow friends"

--- unfollow_friends.js
+++
@@ -1,7 +0,0 @@
-// Script to mass unfollow friends on Facebook
-// Make sure you are in "News Feed Preferences" -> “Unfollow people to hide their posts”
-
-let d = document.getElementsByClassName("_5u3n");
-for (let i = 0; i < d.length; i++){ 
-	d[i].click(); 
-}
