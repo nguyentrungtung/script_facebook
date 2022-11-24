0b06a51f276efb02b182c51861ce4b208fedfed5
tungnt <tungnt.mmo@gmail.com>
2022-11-24 11:49:53 +0700

    cancel_all_intive_like_page

---
+++ cancel_all_intive_like_page.js
@@ -0,0 +1,9 @@
+/**
+ * cancel all intive like page facebook
+ * b1> go to link:  https://m.facebook.com/nt/screen/?params=%7B%7D&path=%2Fpages%2Fpending_invites&state
+ * b2 > Press F12 to open console
+ * b3> coppy code command under in to console
+ */
+
+Object.values(document.getElementsByClassName('_54k8 _52jg _56bs _26vk _86fw _86fx _86fk _86fm _86fe _2rgt _1j-g _2rgt _3zi4 _2rgt _1j-f _2rgt _56bt'))
+  .forEach((button) => button.click());
